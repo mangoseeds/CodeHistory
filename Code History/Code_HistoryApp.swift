7fd075d9f937895832765921bea7f5a1defc9cf0
mangoseeds <juyonbae@gmail.com>
2022-06-20 14:35:38 -0400

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Juyon on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
