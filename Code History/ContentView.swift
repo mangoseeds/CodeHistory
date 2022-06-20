b5383c535df78a2a24e4581d28a362cd38a54eff
mangoseeds <juyonbae@gmail.com>
2022-06-20 14:39:39 -0400

    views

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -2,15 +2,73 @@
 //  ContentView.swift
 //  Code History
 //
-//  Created by Juyon on 6/20/22.
+//  Created by Juyon on 6/18/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack{
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Spacer()
+                
+                HStack{
+                    Button(action: {
+                        print("choice 1")
+                    }, label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("choice 2")
+                    }, label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("choice 3")
+                    }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("choice 4")
+                    }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                }
+            }
+        }
+        .foregroundColor(.white)
     }
 }
 
@@ -19,3 +77,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
