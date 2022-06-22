b5492bb286935f6776e7ea14d1f8df56e6e539b5
mangoseeds <juyonbae@gmail.com>
2022-06-22 14:08:25 -0400

    question model added

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let question = Question(questionText: "What was the first computer bug?", possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"], correctAnswerIndex: 2)
+    
     let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
-    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     var body: some View {
         ZStack {
             mainColor.ignoresSafeArea()
@@ -18,7 +20,7 @@ struct ContentView: View {
                     .font(.callout)
                     .multilineTextAlignment(.leading)
                     .padding()
-                Text("What was the first computer bug?")
+                Text(question.questionText)
                     .font(.largeTitle)
                     .multilineTextAlignment(.leading)
                     .padding()
@@ -27,42 +29,22 @@ struct ContentView: View {
                     Button(action: {
                         print("choice 1")
                     }, label: {
-                        Text("Ant")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
+                        ChoiceTextView(choiceText: question.possibleAnswers[0])
                     })
                     Button(action: {
                         print("choice 2")
                     }, label: {
-                        Text("Beetle")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
+                        ChoiceTextView(choiceText: question.possibleAnswers[1])
                     })
                     Button(action: {
                         print("choice 3")
                     }, label: {
-                        Text("Moth")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
+                        ChoiceTextView(choiceText: question.possibleAnswers[2])
                     })
                     Button(action: {
                         print("choice 4")
                     }, label: {
-                        Text("Fly")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
+                        ChoiceTextView(choiceText: question.possibleAnswers[3])
                     })
                 }
             }
